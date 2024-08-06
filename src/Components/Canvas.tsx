63bec974a652c6ec5146e4a646bb7529668824ce
CGJI58 <leemsk22@naver.com>
2024-08-06 23:58:35 +0900

    canvas test : color change

--- src/Components/Canvas.tsx
+++ src/Components/Canvas.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, FC } from "react";
 import { useRecoilValue } from "recoil";
 import styled from "styled-components";
 import { colorState, widthState } from "../atoms";
@@ -6,39 +6,25 @@ import { colorState, widthState } from "../atoms";
 const Board = styled.canvas`
   width: 400px;
   height: 400px;
-  background-color: rgb(60, 100, 60);
+  background-color: whitesmoke;
   border-radius: 15px;
   box-shadow: 0 4px 60x rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
 `;
 
 function Canvas() {
-  const [context, setContext] = useState<CanvasRenderingContext2D | null>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const width = useRecoilValue(widthState);
   const color = useRecoilValue(colorState);
-
   useEffect(() => {
-    if (canvasRef.current) {
-      const renderCtx = canvasRef.current.getContext("2d");
-
-      if (renderCtx) {
-        setContext(renderCtx);
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const ctx = canvas.getContext("2d");
+      if (ctx) {
+        ctx.fillStyle = color;
+        ctx.fillRect(10, 10, 100, 100);
       }
     }
-  }, [context]);
-  canvasRef.current?.click();
-  return <Board onClick={() => console.log(width, color)} />;
-}
-
-let painting = false;
-let filling = false;
-
-function stopPainting() {
-  painting = false;
-}
-
-function startPainting() {
-  painting = true;
+  }, [color]);
+  return <Board ref={canvasRef} />;
 }
 
 export default Canvas;
