b6b8dd7daecf56eea79bbd28342ed631ef3e1bd1
CGJI58 <leemsk22@naver.com>
2024-08-08 23:58:19 +0900

    canvas : mouse pointer tracking...

--- src/Components/Canvas.tsx
+++ src/Components/Canvas.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useRef, FC } from "react";
+import { useRef, useEffect, FC } from "react";
 import { useRecoilValue } from "recoil";
 import styled from "styled-components";
 import { colorState, modeState, widthState } from "../atoms";
 
-const Board = styled.canvas`
+const Canvas = styled.canvas`
   width: 400px;
   height: 400px;
   background-color: whitesmoke;
@@ -11,27 +11,39 @@ const Board = styled.canvas`
   box-shadow: 0 4px 60x rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
 `;
 
-function Canvas() {
+const CanvasComponent: FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const mousePositionRef = useRef({ x: 0, y: 0 });
   const color = useRecoilValue(colorState);
   const width = useRecoilValue(widthState);
   const mode = useRecoilValue(modeState);
 
+  const handleMouseMove = (event: MouseEvent) => {
+    if (canvasRef.current) {
+      const rect = canvasRef.current.getBoundingClientRect();
+      mousePositionRef.current = {
+        x: event.clientX - rect.left,
+        y: event.clientY - rect.top,
+      };
+    }
+  };
+
   useEffect(() => {
     const canvas = canvasRef.current;
     if (canvas) {
-      const ctx = canvas.getContext("2d");
-      if (ctx) {
-        ctx.fillStyle = color;
-        ctx.lineWidth = width;
-      }
+      canvas.addEventListener("mousemove", handleMouseMove);
     }
-  }, [color]);
+    return () => {
+      if (canvas) {
+        canvas.removeEventListener("mousemove", handleMouseMove);
+      }
+    };
+  }, []);
 
   return (
-    <Board
+    <Canvas
       ref={canvasRef}
-      onMouseMove={onMouseMove}
+      onMouseMove={() => console.log(mousePositionRef.current)}
       onMouseDown={onPainting}
       onMouseUp={stopPainting}
       onMouseLeave={stopPainting}
@@ -39,11 +51,7 @@ function Canvas() {
       onContextMenu={onContextMenu}
     />
   );
-}
-
-function onMouseMove() {
-  console.log("moving...");
-}
+};
 
 function onPainting() {
   console.log("Painting...");
@@ -61,4 +69,4 @@ function onContextMenu() {
   console.log("handleCM...");
 }
 
-export default Canvas;
+export default CanvasComponent;
